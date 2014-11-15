3d520f41c73c0c72814159d8832269f3c2847d91
Harald Codegen <wafuweru@lackmail.net>
2014-11-15 17:25:35 +0100

    Fixed your shit
    Where da hood at

--- project/Assets/Scripts/CustomInputScript.cs
+++ project/Assets/Scripts/CustomInputScript.cs
@@ -1,59 +1,78 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.IO;
 using System.IO.Ports;
-using System.IO;
+using UnityEngine;
 
-public class CustomInputScript : MonoBehaviour {
+public class CustomInputScript : MonoBehaviour
+{
 
-	// Input
-	SerialPort stream;
-	string receivedData = "EMPTY";
-	private bool[] buttonPressed = new bool[6];
+    // Input Method
+    public enum InputMethod { AIvsMouse = 1, BeagleBoard = 2, Keyboard = 3, KeyboardMouse = 4 };
+    public InputMethod inputMethod;
 
-	private CircularBuffer leftSliderBuffer = new CircularBuffer(8);
-	private CircularBuffer rightSliderBuffer = new CircularBuffer(8);
+    // Input
+    SerialPort stream;
+    string receivedData = "EMPTY";
+    //private bool[] buttonPressed = new bool[6];
 
-	private float leftPaddlePosLastFrame = 0f, rightPaddlePosLastFrame = 0f;
-	private float leftPaddleSpeed = 0f, rightPaddleSpeed = 0f;
+    private CircularBuffer leftSliderBuffer = new CircularBuffer(8);
+    private CircularBuffer rightSliderBuffer = new CircularBuffer(8);
+
+    private float leftPaddlePosLastFrame = 0f, rightPaddlePosLastFrame = 0f;
+    private float leftPaddleSpeed = 0f, rightPaddleSpeed = 0f;
 
     //Keyboard input
     private const string LEFT_PLAYER_AXIS = "Vertical1";
     private const string RIGHT_PLAYER_AXIS = "Vertical2";
     private float leftValue = 0f, rightValue = 0f;
 
-    void Awake() {
-        try
+    // PongLogic COntact for AI
+    private PongLogic pongLogic;
+
+    void Awake()
+    {
+        if (inputMethod == InputMethod.BeagleBoard)
         {
-            stream = new SerialPort("COM3", 115200);
-            stream.Open();
-        }
-        catch (IOException e) {
-            Debug.Log(e.Message);   
+            try
+            {
+                stream = new SerialPort("COM3", 115200);
+                stream.Open();
+            }
+            catch (IOException e)
+            {
+                Debug.Log(e.Message + "\nCould not access Cactus Controller. Switching to Keyboard/Mouse setup");
+                inputMethod = InputMethod.KeyboardMouse;
+            }
         }
     }
 
-	// Use this for initialization
-	void Start () {
-
-		if (stream.IsOpen) {
-			Debug.Log ("Stream opened");
+    // Use this for initialization
+    void Start()
+    {
+        if (stream != null)
+        {
+            Debug.Log("Stream opened");
             stream.Write("4");
             receivedData = stream.ReadLine();
 
             leftPaddlePosLastFrame = (System.Convert.ToInt32(receivedData.Substring(18, 4), 16) - 2048f) / 2048f;
             rightPaddlePosLastFrame = (System.Convert.ToInt32(receivedData.Substring(13, 4), 16) - 2048) / 2048f;
-		} else {
-			Debug.Log ("Stream not open");
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (!stream.IsOpen)
+        }
+
+        pongLogic = Camera.main.GetComponent<PongLogic>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputMethod >= InputMethod.Keyboard)
         {
             leftValue += Input.GetAxisRaw(LEFT_PLAYER_AXIS) * Time.deltaTime * 4.5f;
-            rightValue = (Input.mousePosition.y / (float)Screen.height) * 2 - 1;
-            
+
+            if (inputMethod == InputMethod.Keyboard)
+                rightValue += Input.GetAxisRaw(RIGHT_PLAYER_AXIS) * Time.deltaTime * 4.5f;
+            else
+                rightValue = (Input.mousePosition.y / (float)Screen.height) * 2 - 1;
+
 
             leftValue = Mathf.Clamp(leftValue, -1f, 1f);
             rightValue = Mathf.Clamp(rightValue, -1, 1f);
@@ -63,68 +82,96 @@ public class CustomInputScript : MonoBehaviour {
 
             leftPaddlePosLastFrame = leftValue;
             rightPaddlePosLastFrame = rightValue;
-
-            return;
-
         }
-		//get Input
-		stream.Write ("4");
-		receivedData = stream.ReadLine ();
-		
-		float leftSlider = (System.Convert.ToInt32(receivedData.Substring (18, 4), 16) - 2048f) / 2048f;
-		float rightSlider = (System.Convert.ToInt32(receivedData.Substring (13, 4), 16) - 2048) / 2048f;
-		leftSliderBuffer.Add (leftSlider);
-		rightSliderBuffer.Add (rightSlider);
+        else if (inputMethod == InputMethod.BeagleBoard)
+        {
+            stream.Write("4");
+            receivedData = stream.ReadLine();
+
+            float leftSlider = (System.Convert.ToInt32(receivedData.Substring(18, 4), 16) - 2048f) / 2048f;
+            float rightSlider = (System.Convert.ToInt32(receivedData.Substring(13, 4), 16) - 2048) / 2048f;
+            leftSliderBuffer.Add(leftSlider);
+            rightSliderBuffer.Add(rightSlider);
 
 
-		//Calculate Paddle speed
-		float leftSliderPercentage = getLeftSlider ();
-		float rightSliderPercentage = getRightSlider ();
-		
-		leftPaddleSpeed = leftSliderPercentage - leftPaddlePosLastFrame;
-		rightPaddleSpeed = rightSliderPercentage - rightPaddlePosLastFrame;
-		
-		leftPaddlePosLastFrame = leftSliderPercentage;
-		rightPaddlePosLastFrame = rightSliderPercentage;
-	}
+            //Calculate Paddle speed
+            float leftSliderPercentage = getLeftSlider();
+            float rightSliderPercentage = getRightSlider();
 
-	public float getLeftSlider(){
-        if (!stream.IsOpen)
+            leftPaddleSpeed = leftSliderPercentage - leftPaddlePosLastFrame;
+            rightPaddleSpeed = rightSliderPercentage - rightPaddlePosLastFrame;
+
+            leftPaddlePosLastFrame = leftSliderPercentage;
+            rightPaddlePosLastFrame = rightSliderPercentage;
+        }
+        else
+        {
+            GameObject closestBall = null;
+
+            foreach (GameObject ball in pongLogic.getBalls())
+            {
+                if (closestBall == null || closestBall.transform.position.x > ball.transform.position.x)
+                    closestBall = ball;
+            }
+
+            leftValue = (float)closestBall.transform.position.y / pongLogic.getMaxBatPos();
+            rightValue = (Input.mousePosition.y / (float)Screen.height) * 2 - 1;
+
+
+            leftValue = Mathf.Clamp(leftValue, -1f, 1f);
+            rightValue = Mathf.Clamp(rightValue, -1, 1f);
+
+            leftPaddleSpeed = leftValue - leftPaddlePosLastFrame;
+            rightPaddleSpeed = rightValue - rightPaddlePosLastFrame;
+
+            leftPaddlePosLastFrame = leftValue;
+            rightPaddlePosLastFrame = rightValue;
+        }
+    }
+
+    public float getLeftSlider()
+    {
+        if (stream == null)
             return leftValue;
 
-		return leftSliderBuffer.Average ();
-	}
+        return leftSliderBuffer.Average();
+    }
 
-	public float getRightSlider(){
-        if (!stream.IsOpen)
+    public float getRightSlider()
+    {
+        if (stream == null)
             return rightValue;
 
-		return rightSliderBuffer.Average ();
-	}
+        return rightSliderBuffer.Average();
+    }
 
-	public void setMotor(int value){
-        if (!stream.IsOpen)
+    public void setMotor(int value)
+    {
+        if (stream == null)
             return;
 
-		int val = Mathf.Clamp (value, 0, 1000);
+        int val = Mathf.Clamp(value, 0, 1000);
 
-		stream.Write ("m " + value + "\r\n");
-		stream.ReadLine ();
-	}
+        stream.Write("m " + value + "\r\n");
+        stream.ReadLine();
+    }
 
-	public void setLight(int index, int status){
-        if (!stream.IsOpen)
+    public void setLight(int index, int status)
+    {
+        if (stream == null)
             return;
 
-		stream.Write ("l " + index + " " + status + "\r\n");
-		stream.ReadLine ();
-	} 	
+        stream.Write("l " + index + " " + status + "\r\n");
+        stream.ReadLine();
+    }
 
-	public float getLeftSliderSpeed(){
-		return leftPaddleSpeed;		
-	}
+    public float getLeftSliderSpeed()
+    {
+        return leftPaddleSpeed;
+    }
 
-	public float getRightSliderSpeed(){
-		return rightPaddleSpeed;		
-	}
+    public float getRightSliderSpeed()
+    {
+        return rightPaddleSpeed;
+    }
 }
--- project/Assets/Scripts/PongLogic.cs
+++ project/Assets/Scripts/PongLogic.cs
@@ -1,172 +1,198 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.IO.Ports;
+using UnityEngine;
 
 [RequireComponent(typeof(CustomInputScript))]
 public class PongLogic : MonoBehaviour
 {
 
-	public GameObject player1, player2, ballPrefab;
-	public int maxAmountOfBalls;
-	public GameObject[] balls;
-	private List<GameObject> ballsIntern = new List<GameObject> (5);
-	public GameObject upper, lower, left, right; // Walls
-	public TextMesh winText;
-	public ParticleSystem ballHitP1, ballHitP2;
+    public GameObject player1, player2, ballPrefab;
+    public int maxAmountOfBalls;
+    public GameObject[] balls;
+    private List<GameObject> ballsIntern = new List<GameObject>(5);
+    public GameObject upper, lower, left, right; // Walls
+    public TextMesh winText;
+    public ParticleSystem ballHitP1, ballHitP2;
 
     private Game game = Game.getInstance();
 
-	//Ball variablen
-	private float maxBatPos = 3.2f;
-	private float maxAddedYForce = 220f;
-	private float velocityBoost = 1.07f;
-	private float startSpeed = 400f;
-	private float maxSpeed = 20f;
-	private float velocityXtoYRatio = 1.3f;
+    //Ball variablen
+    private float maxBatPos = 3.2f;
+    private float maxAddedYForce = 220f;
+    private float velocityBoost = 1.07f;
+    private float startSpeed = 400f;
+    private float maxSpeed = 20f;
+    private float velocityXtoYRatio = 1.3f;
+    private float minVelocity = 6f;
 
-	//Wie stark der motor rumbled bei einem auftreffen vom ball
-	public int rumblePower = 1000;
+    // Ball Splitting
+    private float timeToBallIdleSplit = 10f;
+    private float durationToBallSplit = 1f;
 
-	//Drall vom Ball
-	public float velocityToSpinRatio = 1.5f;
-	private List<float> ballSpin = new List<float>(5);
-	private float spinFalloff = 0.99f;
+    //Wie stark der motor rumbled bei einem auftreffen vom ball
+    public int rumblePower = 1000;
 
-	// Input
-	private CustomInputScript customInput;
+    //Drall vom Ball
+    public float velocityToSpinRatio = 1.5f;
+    private List<float> ballSpin = new List<float>(5);
+    private float spinFalloff = 0.99f;
 
-	void Start ()
-	{
-		customInput = GetComponent<CustomInputScript> ();
-		
-		foreach (GameObject g in balls) {
-			if (g) {
-				ballsIntern.Add (g);
-				g.rigidbody.AddForce (Vector3.right * -startSpeed);
-				startSpeed *= -1;
-				ballSpin.Add(0);
-			}
-		}
-	}
+    // Input
+    private CustomInputScript customInput;
 
-	// Update is called once per frame
-	void Update ()
-	{
+    void Start()
+    {
+        customInput = GetComponent<CustomInputScript>();
 
-		float leftSliderPercentage = customInput.getLeftSlider ();
-		float rightSliderPercentage = customInput.getRightSlider ();
-
-		float leftPlayerSpeed = customInput.getLeftSliderSpeed ();
-		float rightPlayerSpeed = customInput.getRightSliderSpeed ();
-		
-
-		player1.transform.position = new Vector3 (player1.transform.position.x, leftSliderPercentage * maxBatPos, player1.transform.position.z);
-		player2.transform.position = new Vector3 (player2.transform.position.x, rightSliderPercentage * maxBatPos, player2.transform.position.z);
-		
-
-		for (int i = ballsIntern.Count - 1; i >= 0; i--) {
-			GameObject ball = ballsIntern[i];
-
-			Vector3 newVel = Vector3.Normalize (ball.rigidbody.velocity + new Vector3 (0, ballSpin [i], 0));
-			float ballSpeed = ball.rigidbody.velocity.magnitude;
-			ball.rigidbody.velocity = newVel * ballSpeed;
-			ballSpin [i] *= spinFalloff;
-
-
-			if (hasHitLeftPlayer(ball)) {
-
-				float angleForce = ((player1.renderer.bounds.max.y - ball.transform.position.y - player1.renderer.bounds.size.y / 2f) / (player1.renderer.bounds.size.y / 2f)) * -maxAddedYForce;
-
-				ball.rigidbody.AddForce (Vector3.up * angleForce);
-				ball.rigidbody.velocity = new Vector3 (Mathf.Min (Mathf.Abs (ball.rigidbody.velocity.x) * velocityBoost, maxSpeed), Mathf.Sign (ball.rigidbody.velocity.y) * Mathf.Min (Mathf.Abs (ball.rigidbody.velocity.y), Mathf.Abs (velocityXtoYRatio * ball.rigidbody.velocity.x)), 0);
-
-				Instantiate (ballHitP1, ball.transform.position, ballHitP1.transform.rotation);
-				StartCoroutine ("Rumble");
-
-				ballSpin [i] = leftPlayerSpeed * velocityToSpinRatio;
-			}
-
-			if (hasHitRightPlayer(ball)) {
-					
-				float angleForce = ((player2.renderer.bounds.max.y - ball.transform.position.y - player2.renderer.bounds.size.y / 2f) / (player2.renderer.bounds.size.y / 2f)) * -maxAddedYForce;
-					
-				ball.rigidbody.AddForce (Vector3.up * angleForce);
-				ball.rigidbody.velocity = new Vector3 (Mathf.Max (Mathf.Abs (ball.rigidbody.velocity.x) * -1 * velocityBoost, -maxSpeed), Mathf.Sign (ball.rigidbody.velocity.y) * Mathf.Min (Mathf.Abs (ball.rigidbody.velocity.y), Mathf.Abs (velocityXtoYRatio * ball.rigidbody.velocity.x)), 0);
-
-				Instantiate (ballHitP2, ball.transform.position, ballHitP2.transform.rotation);
-				StartCoroutine ("Rumble");	
-
-				ballSpin [i] = rightPlayerSpeed * velocityToSpinRatio;
-			}
-
-			if (ball.renderer.bounds.max.y >= upper.renderer.bounds.min.y) {
-				ball.rigidbody.velocity = new Vector3 (ball.rigidbody.velocity.x, Mathf.Abs (ball.rigidbody.velocity.y) * -1, 0);
-			}
-
-			if (ball.renderer.bounds.min.y <= lower.renderer.bounds.max.y) {
-				ball.rigidbody.velocity = new Vector3 (ball.rigidbody.velocity.x, Mathf.Abs (ball.rigidbody.velocity.y), 0);
-			}
-
-			if (ball.renderer.bounds.min.x <= left.renderer.bounds.min.x) { // eigtl bound max, aber buggt
-
-				ballsIntern.Remove (ball);
-				ballSpin.RemoveAt(i);
-				Destroy (ball);
-                Game.getInstance().player2GameScore++; 
-			} else if (ball.renderer.bounds.max.x >= right.renderer.bounds.max.x) { // eigtl bound min, aber buggt
-
-				ballsIntern.Remove (ball);	
-				ballSpin.RemoveAt(i);
-				Destroy (ball);
-                Game.getInstance().player1GameScore++; 
-			}
-
-		}
-		
-		if (ballsIntern.Count == 0) {
-            if (game.player1GameScore > game.player2GameScore)
-				winText.text = "Player 1 wins";
-            else if (game.player2GameScore > game.player1GameScore)
-				winText.text = "Player 2 wins";
-			else
-				winText.text = "Navab wins";
-
-			StartCoroutine ("GameOver");
+        foreach (GameObject g in balls)
+        {
+            if (g)
+            {
+                ballsIntern.Add(g);
+                g.rigidbody.AddForce(Vector3.right * -startSpeed);
+                startSpeed *= -1;
+                ballSpin.Add(0);
+            }
         }
-        else if (ballsIntern.Count == 1 &&  !idleBalloonRunning ) {
+
+        Screen.showCursor = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float leftSliderPercentage = customInput.getLeftSlider();
+        float rightSliderPercentage = customInput.getRightSlider();
+
+        float leftPlayerSpeed = customInput.getLeftSliderSpeed();
+        float rightPlayerSpeed = customInput.getRightSliderSpeed();
+
+
+        player1.transform.position = new Vector3(player1.transform.position.x, leftSliderPercentage * maxBatPos, player1.transform.position.z);
+        player2.transform.position = new Vector3(player2.transform.position.x, rightSliderPercentage * maxBatPos, player2.transform.position.z);
+
+
+        for (int i = ballsIntern.Count - 1; i >= 0; i--)
+        {
+            GameObject ball = ballsIntern[i];
+
+            Vector3 newVel = Vector3.Normalize(ball.rigidbody.velocity + new Vector3(0, ballSpin[i], 0));
+            float ballSpeed = ball.rigidbody.velocity.magnitude;
+            ball.rigidbody.velocity = newVel * ballSpeed;
+            ballSpin[i] *= spinFalloff;
+
+
+            if (hasHitLeftPlayer(ball))
+            {
+
+                float angleForce = ((player1.renderer.bounds.max.y - ball.transform.position.y - player1.renderer.bounds.size.y / 2f) / (player1.renderer.bounds.size.y / 2f)) * -maxAddedYForce;
+
+                ball.rigidbody.AddForce(Vector3.up * angleForce);
+                ball.rigidbody.velocity = new Vector3(Mathf.Min(Mathf.Abs(ball.rigidbody.velocity.x) * velocityBoost, maxSpeed), Mathf.Sign(ball.rigidbody.velocity.y) * Mathf.Min(Mathf.Abs(ball.rigidbody.velocity.y), Mathf.Abs(velocityXtoYRatio * ball.rigidbody.velocity.x)), 0);
+
+                Instantiate(ballHitP1, ball.transform.position, ballHitP1.transform.rotation);
+                StartCoroutine("Rumble");
+
+                ballSpin[i] = leftPlayerSpeed * velocityToSpinRatio;
+            }
+
+            if (hasHitRightPlayer(ball))
+            {
+
+                float angleForce = ((player2.renderer.bounds.max.y - ball.transform.position.y - player2.renderer.bounds.size.y / 2f) / (player2.renderer.bounds.size.y / 2f)) * -maxAddedYForce;
+
+                ball.rigidbody.AddForce(Vector3.up * angleForce);
+                ball.rigidbody.velocity = new Vector3(Mathf.Max(Mathf.Abs(ball.rigidbody.velocity.x) * -1 * velocityBoost, -maxSpeed), Mathf.Sign(ball.rigidbody.velocity.y) * Mathf.Min(Mathf.Abs(ball.rigidbody.velocity.y), Mathf.Abs(velocityXtoYRatio * ball.rigidbody.velocity.x)), 0);
+
+                Instantiate(ballHitP2, ball.transform.position, ballHitP2.transform.rotation);
+                StartCoroutine("Rumble");
+
+                ballSpin[i] = rightPlayerSpeed * velocityToSpinRatio;
+            }
+
+            if (ball.renderer.bounds.max.y >= upper.renderer.bounds.min.y)
+            {
+                ball.rigidbody.velocity = new Vector3(ball.rigidbody.velocity.x, Mathf.Abs(ball.rigidbody.velocity.y) * -1, 0);
+            }
+
+            if (ball.renderer.bounds.min.y <= lower.renderer.bounds.max.y)
+            {
+                ball.rigidbody.velocity = new Vector3(ball.rigidbody.velocity.x, Mathf.Abs(ball.rigidbody.velocity.y), 0);
+            }
+
+            if (ball.renderer.bounds.min.x <= left.renderer.bounds.min.x)
+            { // eigtl bound max, aber buggt
+
+                ballsIntern.Remove(ball);
+                ballSpin.RemoveAt(i);
+                Destroy(ball);
+                Game.getInstance().player2GameScore++;
+            }
+            else if (ball.renderer.bounds.max.x >= right.renderer.bounds.max.x)
+            { // eigtl bound min, aber buggt
+
+                ballsIntern.Remove(ball);
+                ballSpin.RemoveAt(i);
+                Destroy(ball);
+                Game.getInstance().player1GameScore++;
+            }
+
+            if (ball.rigidbody.velocity.magnitude < minVelocity)
+            {
+                ball.rigidbody.velocity *= (minVelocity / ball.rigidbody.velocity.magnitude);
+            }
+        }
+
+        if (ballsIntern.Count == 0)
+        {
+            if (game.player1GameScore > game.player2GameScore)
+                winText.text = "Player 1 wins";
+            else if (game.player2GameScore > game.player1GameScore)
+                winText.text = "Player 2 wins";
+            else
+                winText.text = "Navab wins";
+
+            StartCoroutine("GameOver");
+        }
+        else if (ballsIntern.Count == 1 && !idleBalloonRunning)
+        {
             StartCoroutine("IdleBalloon");
         }
 
-	}
+    }
 
-	private bool hasHitLeftPlayer(GameObject ball){
-		return 	ball.renderer.bounds.min.x <= player1.renderer.bounds.max.x &&
-				ball.transform.position.y <= player1.renderer.bounds.max.y &&
-				ball.transform.position.y >= player1.renderer.bounds.min.y;
-	}
+    private bool hasHitLeftPlayer(GameObject ball)
+    {
+        return ball.renderer.bounds.min.x <= player1.renderer.bounds.max.x &&
+                ball.transform.position.y <= player1.renderer.bounds.max.y &&
+                ball.transform.position.y >= player1.renderer.bounds.min.y;
+    }
 
-	private bool hasHitRightPlayer(GameObject ball){
-		return 	ball.renderer.bounds.max.x >= player2.renderer.bounds.min.x &&
-				ball.transform.position.y <= player2.renderer.bounds.max.y &&
-				ball.transform.position.y >= player2.renderer.bounds.min.y;	
-	}
+    private bool hasHitRightPlayer(GameObject ball)
+    {
+        return ball.renderer.bounds.max.x >= player2.renderer.bounds.min.x &&
+                ball.transform.position.y <= player2.renderer.bounds.max.y &&
+                ball.transform.position.y >= player2.renderer.bounds.min.y;
+    }
 
     public AnimationCurve slowCurve;
     private bool idleBalloonRunning = false;
-	private IEnumerator IdleBalloon ()
-	{
+    private IEnumerator IdleBalloon()
+    {
         idleBalloonRunning = true;
-		yield return new WaitForSeconds (1f);
+        yield return new WaitForSeconds(timeToBallIdleSplit);
 
-        while (ballsIntern.Count == 1 && !isMovingTowardCenter(ballsIntern[0])) { 
+        while (ballsIntern.Count == 1 && !isMovingTowardCenter(ballsIntern[0]))
+        {
             yield return new WaitForSeconds(0.1f);
         }
 
-        if (ballsIntern.Count == 1) { 
+        if (ballsIntern.Count == 1)
+        {
             GameObject ball = ballsIntern[0];
             ball.GetComponent<TrailRenderer>().enabled = false;
-            float startVel = ball.rigidbody.velocity.magnitude;
+            float startVel = ball.rigidbody.velocity.magnitude * 0.5f;
             float startX = ball.transform.position.x;
 
             while (Mathf.Abs(ball.transform.position.x) >= 0.1f)
@@ -178,7 +204,7 @@ public class PongLogic : MonoBehaviour
             ball.transform.position = new Vector3(0, ball.transform.position.y, 0);
             ball.rigidbody.velocity = new Vector3(0, 0, 0);
 
-            yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(durationToBallSplit);
 
             AddBall(new Vector3(0.5f, ball.transform.position.y, 0f), new Vector3(startVel, 0f, 0f));
 
@@ -188,76 +214,80 @@ public class PongLogic : MonoBehaviour
 
             idleBalloonRunning = false;
         }
-	}
-
-    private bool isMovingTowardCenter(GameObject ball) { 
-        return (ball.transform.position.x > 0 && ball.rigidbody.velocity.x < 0) || (ball.transform.position.x < 0 && ball.rigidbody.velocity.x > 0);
-            // is the ball on the right side and moving to the left OR is the ball on the left side and movign to the right
     }
 
-	private bool gameOver = false;
+    private bool isMovingTowardCenter(GameObject ball)
+    {
+        return (ball.transform.position.x > 0 && ball.rigidbody.velocity.x < 0) || (ball.transform.position.x < 0 && ball.rigidbody.velocity.x > 0);
+        // is the ball on the right side and moving to the left OR is the ball on the left side and movign to the right
+    }
 
-	private IEnumerator GameOver ()
-	{
-		if (!gameOver) {
-			gameOver = true;
+    private bool gameOver = false;
 
-			for (int i = 0; i < 3; i++) {
+    private IEnumerator GameOver()
+    {
+        if (!gameOver)
+        {
+            gameOver = true;
 
-				customInput.setLight (0, 1);
+            for (int i = 0; i < 3; i++)
+            {
 
-				yield return new WaitForSeconds (0.1f);
+                customInput.setLight(0, 1);
 
-				customInput.setLight (0, 0);
+                yield return new WaitForSeconds(0.1f);
 
-				customInput.setLight (1, 1);
+                customInput.setLight(0, 0);
 
-				yield return new WaitForSeconds (0.1f);
+                customInput.setLight(1, 1);
 
-				customInput.setLight (1, 0);
+                yield return new WaitForSeconds(0.1f);
 
-				customInput.setLight (2, 1);
+                customInput.setLight(1, 0);
 
-				yield return new WaitForSeconds (0.1f);
+                customInput.setLight(2, 1);
 
-				customInput.setLight (2, 0);
+                yield return new WaitForSeconds(0.1f);
 
-				customInput.setLight (3, 1);
+                customInput.setLight(2, 0);
 
-				yield return new WaitForSeconds (0.1f);
+                customInput.setLight(3, 1);
 
-				customInput.setLight (3, 0);
-			}
+                yield return new WaitForSeconds(0.1f);
 
-			yield return new WaitForSeconds (1);
+                customInput.setLight(3, 0);
+            }
+
+            yield return new WaitForSeconds(1);
             game.player1GameScore = 0;
             game.player2GameScore = 0;
 
-			Application.LoadLevel ("pong");
-		}
-	}
+            Application.LoadLevel("pong");
+        }
+    }
 
-	private IEnumerator Rumble ()
-	{
-		customInput.setMotor (rumblePower);
+    private IEnumerator Rumble()
+    {
+        customInput.setMotor(rumblePower);
 
-		yield return new WaitForSeconds (0.2f);
+        yield return new WaitForSeconds(0.2f);
 
-		customInput.setMotor (0);
-	}
+        customInput.setMotor(0);
+    }
 
-	public GameObject AddBall (Vector3 pos)
-	{
-		if (ballsIntern.Count < maxAmountOfBalls) {
-			ballSpin.Add(0);
-			GameObject ball = (GameObject)Instantiate (ballPrefab, pos, ballPrefab.transform.rotation);
-			ballsIntern.Add (ball);
-			ball.rigidbody.AddForce (Vector3.right * -startSpeed);
-			startSpeed *= -1;
-			return ball;
-		}
-		return null;
-	}
+    public GameObject AddBall(Vector3 pos)
+    {
+        if (ballsIntern.Count < maxAmountOfBalls)
+        {
+            ballSpin.Add(0);
+            GameObject ball = (GameObject)Instantiate(ballPrefab, pos, ballPrefab.transform.rotation);
+            ballsIntern.Add(ball);
+            ball.rigidbody.AddForce(Vector3.right * -startSpeed);
+            startSpeed *= -1;
+            return ball;
+        }
+        return null;
+    }
 
     public GameObject AddBall(Vector3 pos, Vector3 vel)
     {
@@ -273,17 +303,23 @@ public class PongLogic : MonoBehaviour
         return null;
     }
 
-	void OnApplicationQuit ()
-	{
-		customInput.setMotor (0);
+    void OnApplicationQuit()
+    {
+        customInput.setMotor(0);
 
-		customInput.setLight (0, 0);
-		customInput.setLight (1, 0);
-		customInput.setLight (2, 0);
-		customInput.setLight (3, 0);
-	}
+        customInput.setLight(0, 0);
+        customInput.setLight(1, 0);
+        customInput.setLight(2, 0);
+        customInput.setLight(3, 0);
+    }
 
-    public List<GameObject> getBalls() {
+    public List<GameObject> getBalls()
+    {
         return ballsIntern;
+    }
+
+    public float getMaxBatPos()
+    {
+        return maxBatPos;
     }
 }
